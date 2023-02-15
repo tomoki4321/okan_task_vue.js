65de410cbe4d25e3daca881d043815e82e9e1f5c
tomoki <benzousan7@gmail.com>
2023-02-15 11:29:59 +0900

    一覧ページを作成

--- src/views/todo/ListTodo.vue
+++ src/views/todo/ListTodo.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import { useAuthStore } from "@/stores/auth";
+import axios from "axios";
+const authStore = useAuthStore();
+const index = reactive({
+  todos: [],
+});
+TodoListUp();
+async function TodoListUp(): Promise<void> {
+  await axios
+    .get("http://localhost:3000/api/v1/tasks", {
+      headers: {
+        uid: authStore.uid,
+        "access-token": authStore.access_token,
+        client: authStore.client,
+      },
+    })
+    .then((response) => {
+      index.todos = response.data;
+      console.log(response.data);
+    });
+}
+</script>
+
 <template>
-  <h1>タスク一覧</h1>
+  <div>
+    <ul>
+      <li v-for="todo in index.todos" :key="todo.id">
+        タスク名:{{ todo.name }}
+        タスク内容:{{ todo.content }}
+      </li>
+    </ul>
+  </div>
 </template>
