5dec82739239798426051b6865154fda908ce141
tomoki <benzousan7@gmail.com>
2026-06-18 15:40:50 +0900

    ロゴを更新

--- src/stores/flash-message.ts
+++ src/stores/flash-message.ts
@@ -5,6 +5,7 @@ export const useFlashMessageStore = defineStore("message", () => {
   const text = ref("");
   function flash(this: any, message: string): void {
     this.text = message;
+    window.scrollTo({ top: 0, behavior: "smooth" });
     setTimeout(() => {
       this.text = "";
     }, 5000);
